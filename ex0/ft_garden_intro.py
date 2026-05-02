90b4f44a0a8e6ab20519da1c1f28f7811caf2dac
Saba <egribassaba@gmail.com>
2026-05-02 22:39:17 +0200

    ran flake8 and mypy over .py

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 if __name__ == "__main__":
-	print("=== Welcome to My Garden ===")
-	name = "Rose"
-	print(f"Plant: {name}")
-	height = "25cm"
-	print(f"Height: {height}")
-	age = "30 days"
-	print(f"Age: {age}")
-	print("=== End of Program ===")
+    print("=== Welcome to My Garden ===")
+    name = "Rose"
+    print(f"Plant: {name}")
+    height = "25cm"
+    print(f"Height: {height}")
+    age = "30 days"
+    print(f"Age: {age}")
+    print("=== End of Program ===")
--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,28 +1,31 @@
+#!/usr/bin/env python3
 class Plant:
-	def __init__(self):
-		self.name: str = ""
-		self.height: float = 0.0
-		self.age: int = 0
-	def show(self):
-		print(f"{self.name}: {self.height}cm, {self.age} days old")
+    def __init__(self) -> None:
+        self.name: str = ""
+        self.height: float = 0.0
+        self.age: int = 0
+
+    def show(self) -> None:
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
 
 if __name__ == "__main__":
-	print("=== Garden Plant Registry ===")
-	rose = Plant()
-	rose.name = "Rose"
-	rose.height = 25
-	rose.age = 30
+    print("=== Garden Plant Registry ===")
+    rose = Plant()
+    rose.name = "Rose"
+    rose.height = 25
+    rose.age = 30
 
-	sunflower = Plant()
-	sunflower.name = "Sunflower"
-	sunflower.height = 80
-	sunflower.age = 45
+    sunflower = Plant()
+    sunflower.name = "Sunflower"
+    sunflower.height = 80
+    sunflower.age = 45
 
-	cactus = Plant()
-	cactus.name = "Cactus"
-	cactus.height = 15
-	cactus.age = 120
+    cactus = Plant()
+    cactus.name = "Cactus"
+    cactus.height = 15
+    cactus.age = 120
 
-	rose.show()
-	sunflower.show()
-	cactus.show()
+    rose.show()
+    sunflower.show()
+    cactus.show()
